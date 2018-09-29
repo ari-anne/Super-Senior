2a3956d092ba8625ac19d921eccc418602553fc7
ari <ari@localhost.localdomain>
2018-09-29 09:46:33 -0700

    main menu

---
+++ SuperSenior/app/src/main/java/com/example/ari/supersenior/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ari.supersenior;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
