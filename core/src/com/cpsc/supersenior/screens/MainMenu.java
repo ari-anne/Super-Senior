93fcc1e015043350c47ffea8b3baf68f63909bb2
ari-anne <aapa.arin@hotmail.com>
2018-10-20 22:01:39 -0700

    finished main menu

--- core/src/com/cpsc/supersenior/screens/MainMenu.java
+++ core/src/com/cpsc/supersenior/screens/MainMenu.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
@@ -15,11 +14,10 @@ public class MainMenu implements Screen{
 
     private SuperSenior game;
     private Stage stage = new Stage();
-    // Labels   https://libgdx.info/basic-label/
-    // Skins    https://github.com/libgdx/libgdx/wiki/Skin#skin-json
-    // Tables   https://github.com/EsotericSoftware/tablelayout
-    // Buttons  https://stackoverflow.com/questions/21488311/how-to-create-a-button-in-libgdx
-
+    // Labels       https://libgdx.info/basic-label/
+    // Skins        https://github.com/libgdx/libgdx/wiki/Skin#skin-json
+    // Tables       https://github.com/EsotericSoftware/tablelayout
+    // ButtonSkin   https://stackoverflow.com/questions/21488311/how-to-create-a-button-in-libgdx
 
 
     public MainMenu (SuperSenior game){
@@ -27,69 +25,57 @@ public class MainMenu implements Screen{
     }
 
 
-
     @Override
     public void show() {
         Gdx.input.setInputProcessor(stage);
-        Skin skin = new Skin(Gdx.files.internal("sgx/skin/sgx-ui.json"));
-
-        // BACKGROUND
-        Texture texture = new Texture(Gdx.files.internal("game_background_1.png"));
-        TextureRegion textureRegion = new TextureRegion(texture);
-        Image bg = new Image(textureRegion);
-        bg.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        bg.setPosition(0, Gdx.graphics.getHeight()-bg.getHeight());
-        stage.addActor(bg);
-
-        // SET UP TABLE
+        Skin skin = new Skin(Gdx.files.internal("buttons/button.json"));
         Table table = new Table();
-        table.setFillParent(true);
-        //table.debug();
-        stage.addActor(table);
 
-        // TITLE
-        Label title = new Label("Super Senior", skin, "title");
-        stage.addActor(title);
-        table.add(title).expandX().pad(100);
-        table.row();
+        Texture texture = new Texture(Gdx.files.internal("backgrounds/game_background_1/game_background_1.png"));
+        Image background = new Image(texture);
+        TextButton title = new TextButton("  Super Senior  ", skin);
+        Button play = new Button(skin, "play");
+        Button high_scores = new Button(skin, "high-scores");
+        Button settings = new Button(skin, "settings");
 
-        // PLAY BUTTON
-        TextButton play = new TextButton("Play", skin);
-        play.setSize(150, 50);
+        int padding = 100;
+
+        background.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        background.setPosition(0, Gdx.graphics.getHeight()-background.getHeight());
+
         play.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 game.setScreen(new GameScreen(game));
             }
         });
-        stage.addActor(play);
-        table.add(play).expandX().pad(100);
-        table.row();
-
-        // HIGH SCORES BUTTON
-        TextButton high_scores = new TextButton("High Scores", skin);
-        high_scores.setSize(150, 50);
         high_scores.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 game.setScreen(new HighScores(game));
             }
         });
-        stage.addActor(high_scores);
-        table.add(high_scores).expandX().pad(100);
-        table.row();
-
-        // SETTINGS BUTTON
-        TextButton settings = new TextButton("Settings", skin);
-        settings.setSize(150, 50);
         settings.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 game.setScreen(new Settings(game));
             }
         });
+
+        stage.addActor(background);
+        stage.addActor(table);
+        stage.addActor(title);
+        stage.addActor(play);
+        stage.addActor(high_scores);
         stage.addActor(settings);
-        table.add(settings).expandX().pad(100);
+
+        table.setFillParent(true);
+        table.row().height(500);
+        table.add(title).colspan(3).padTop(padding);
+        table.row();
+        table.add(play).pad(padding);
+        table.add(high_scores).pad(padding);
+        table.add(settings).pad(padding);
     }
 
 
